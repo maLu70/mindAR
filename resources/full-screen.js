830a72a2111d4cf1b9844b15dad9a9ff126dc633
luisMigue-lm <tavaresalvesluismiguel@gmail.com>
2025-11-11 08:35:18 -0300

    fullscreen

--- resources/full-screen.js
+++ resources/full-screen.js
@@ -1,11 +1,2 @@
-const corpo = document.getElementsByTagName(body)
-
-if(corpo.requestFullscreem) {
-    corpo.requestFullscreem();
-} else if (corpo.webkitRequestFullScreen) {
-    corpo.webkitRequestFullScreen();
-} else if (corpo.mozRequestFullScreen) {
-    corpo.mozRequestFullScreen();
-} else {
-    console.log("não funciona")
-}
+// testar
+//https://concepcaoweb.com.br/fullscreem-com-javascript/
