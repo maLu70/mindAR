1bd20522e133d5432804bbd72a481d6b52204138
luisMigue-lm <tavaresalvesluismiguel@gmail.com>
2025-11-13 08:01:01 -0300

    teste do fullscreen

--- resources/full-screen.js
+++ resources/full-screen.js
@@ -1,2 +1,63 @@
 // testar
 //https://concepcaoweb.com.br/fullscreem-com-javascript/
+
+"use strict"
+let fullScreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
+let fullsScreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled;
+
+document.getElementsById('teste').click();
+let tools;
+let trigger;
+let isFull;
+let method;
+let closed;
+let trigger_w;
+
+//-- Funções
+let enterFullScreen;
+let exitFullScreen;
+let isFullScreen;
+
+    function toggleFullScreen(tela, callBack) {
+    isFull = false;
+    method = null;
+
+    tela.style.position = "absolute";
+
+    if (tela.requestFullscreen) {
+        tela.requestFullscreen();
+        isFull = true;
+        method = "";
+
+    } else if (tela.msRequestFullscreen) {
+        tela.msRequestFullscreen();
+        isFull = true;
+            method = "ms";
+
+    } else if (tela.mozRequestFullscreen) {
+        tela.mozRequestFullscreen();
+        isFull = true;
+        method = "moz";
+
+    } else if (tela.webktiRequestFullscreen) {
+        tela.msRequestFullscreen();
+        isFull = true;
+        method = "webkit";
+
+    } else {
+        console.log("Não tem jeito, paizão")
+    }
+
+    if (isFull) {
+        if (callBack !=  undefined) callBack();
+        
+    }
+}
+
+isFullScreen = function() {
+    return document.fullscreen ||
+           document.msFullScreen ||
+           document.mozFullScreen ||
+           document.webkitFullScreen;
+}
+
